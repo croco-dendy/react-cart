11268ef42612c0dfba0b9909594ed5f38487be6e
Denis Feofilactov <denis.feofilactov@gmail.com>
2021-02-16 15:49:15 +0200

    Update Routes.tsx

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -7,6 +7,8 @@ const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/cart" component={Cart} />
+    {/* GitHub pages */}
+    <Route exact path="/react-cart" component={Home} />
   </Switch>
 );
 
